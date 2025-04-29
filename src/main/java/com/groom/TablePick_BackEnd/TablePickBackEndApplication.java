c878471c59e84dfc6fe817562a7d4fb22efcc7c4
jihye <jihye07201@gmail.com>
2025-04-29 11:07:15 +0900

    Initial commit

---
+++ src/main/java/com/groom/TablePick_BackEnd/TablePickBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.groom.TablePick_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablePickBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablePickBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groom/TablePick_BackEnd/TablePickBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groom.TablePick_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablePickBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
