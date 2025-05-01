89a79efaff45858b9a1763bf46bd3855bcf557b5
OHEUNSOL <eunsol037@naver.com>
2025-05-01 01:42:09 -0700

    테스트 디렉토리 이름 수정

--- src/test/java/com/goorm/tablepick/TablePickBackEndApplicationTests.java
+++ src/test/java/com/goorm/tablepick/TablePickBackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package com.groom.TablePick_BackEnd;
+package com.goorm.tablepick;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TablePickBackEndApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
