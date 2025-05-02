24bbf9d1c4b4b1bb0be418ec8f6e0dc24d270a65
OHEUNSOL <eunsol037@naver.com>
2025-05-01 22:15:43 -0700

    TP-19: test

--- src/main/java/com/goorm/tablepick/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/goorm/tablepick/domain/reservation/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.goorm.tablepick.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
