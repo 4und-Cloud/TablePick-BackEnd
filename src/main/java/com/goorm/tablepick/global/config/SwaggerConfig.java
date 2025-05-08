24a6bfbdf6dc1132c6f5ae49b6022646c0a7c0c1
jihye <jihye07201@gmail.com>
2025-05-08 16:23:44 +0900

    TP-46 swagger 설정 추가

--- src/main/java/com/goorm/tablepick/global/config/SwaggerConfig.java
+++ src/main/java/com/goorm/tablepick/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.goorm.tablepick.global.config;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,20 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("TablePick API 문서")
                         .version("v1.0.0")
-                        .description("테이블 예약, 식당 관리, 결제 등 API 명세서입니다."));
+                        .description("테이블 예약, 식당 관리, 결제 등 API 명세서입니다."))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
