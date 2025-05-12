da36d77d0e095084eb374a60341246a221820afb
jihye <jihye07201@gmail.com>
2025-05-12 10:20:42 +0900

    TP-44 식당 검색 API
    restaurantService 구현했습니다.

---
+++ src/main/java/com/goorm/tablepick/domain/restaurant/service/RestaurantService.java
@@ -0,0 +1,12 @@
+package com.goorm.tablepick.domain.restaurant.service;
+
+import com.goorm.tablepick.domain.restaurant.dto.request.RestaurantCategorySearchRequestDto;
+import com.goorm.tablepick.domain.restaurant.dto.request.RestaurantKeywordSearchRequestDto;
+import com.goorm.tablepick.domain.restaurant.dto.response.PagedRestaurantResponseDto;
+import jakarta.validation.Valid;
+
+public interface RestaurantService {
+    PagedRestaurantResponseDto searchAllByKeyword(@Valid RestaurantKeywordSearchRequestDto keywordSearchRequestDto);
+
+    PagedRestaurantResponseDto searchAllByCategory(@Valid RestaurantCategorySearchRequestDto categorySearchRequestDto);
+}
