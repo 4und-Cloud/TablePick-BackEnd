ebe5a3289d13046592996dd1949bd35a0e83ba0d
jihye <jihye07201@gmail.com>
2025-05-17 18:21:08 +0900

    TP-104 태그 조회 API

--- src/main/java/com/goorm/tablepick/domain/tag/repository/TagRepository.java
+++ src/main/java/com/goorm/tablepick/domain/tag/repository/TagRepository.java
@@ -1,10 +1,9 @@
 package com.goorm.tablepick.domain.tag.repository;
 
 import com.goorm.tablepick.domain.tag.entity.Tag;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
