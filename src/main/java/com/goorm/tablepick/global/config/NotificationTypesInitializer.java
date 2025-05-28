fc247c0b4e16d440fff09c61dbccffe594b1bd60
Kim Jaewon <rekjeawon88@naver.com>
2025-05-28 17:29:55 +0900

    [TP-157] 알림 타이틀 수정

--- src/main/java/com/goorm/tablepick/global/config/NotificationTypesInitializer.java
+++ src/main/java/com/goorm/tablepick/global/config/NotificationTypesInitializer.java
@@ -21,42 +21,42 @@ public class NotificationTypesInitializer implements ApplicationRunner {
         // 항상 모든 알림 타입을 확인하고 필요한 경우 생성
         ensureNotificationTypeExists(
                 NotificationTypes.REGISTER_COMPLETED,
-                "TablePick 테이블픽",
+                "회원가입 완료",
                 "테이블픽에 가입해 주셔서 감사합니다. 앞으로 다양한 혜택과 서비스를 이용해보세요.",
                 ""
         );
 
         ensureNotificationTypeExists(
                 NotificationTypes.RESERVATION_COMPLETED,
-                "TablePick 테이블픽",
+                "식당 예약 완료",
                 "{restaurantName} 예약이 성공적으로 완료되었습니다.",
                 "/reservations/{id}"
         );
 
         ensureNotificationTypeExists(
                 NotificationTypes.RESERVATION_1DAY_BEFORE,
-                "TablePick 테이블픽",
+                "식당 예약날 1일 전",
                 "내일 이 시간에 {restaurantName} 예약이 예정되어 있습니다.",
                 "/reservations/{id}"
         );
 
         ensureNotificationTypeExists(
                 NotificationTypes.RESERVATION_3HOURS_BEFORE,
-                "TablePick 테이블픽",
+                "식당 예약시간 3시간 전",
                 "3시간 뒤 {restaurantName} 예약이 있습니다. 위치와 시간을 다시 한번 확인해 주세요.",
                 "/reservations/{id}"
         );
 
         ensureNotificationTypeExists(
                 NotificationTypes.RESERVATION_1HOUR_BEFORE,
-                "TablePick 테이블픽",
+                "식당 예약시간 1시간 전",
                 "{restaurantName} 예약이 1시간 남았습니다. 매장에서 고객님을 맞을 준비를 하고 있으니 여유 있게 출발해주세요.",
                 "/reservations/{id}"
         );
 
         ensureNotificationTypeExists(
                 NotificationTypes.RESERVATION_3HOURS_AFTER,
-                "TablePick 테이블픽",
+                "식당 리뷰 요청",
                 "{restaurantName}에서의 식사는 만족스러우셨나요? 리뷰를 남겨주시면 큰 도움이 됩니다.",
                 "/reservations/{id}/review"
         );
