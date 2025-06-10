ba9be4d7fbbfbd2694839d40888e395ad0c6acbf
jihye <jihye07201@gmail.com>
2025-06-10 14:04:50 +0900

    [TP-183] 검색 dto 수정

--- src/main/java/com/goorm/tablepick/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/goorm/tablepick/domain/restaurant/service/RestaurantService.java
@@ -2,11 +2,7 @@ package com.goorm.tablepick.domain.restaurant.service;
 
 import com.goorm.tablepick.domain.restaurant.dto.request.RestaurantCategorySearchRequestDto;
 import com.goorm.tablepick.domain.restaurant.dto.request.RestaurantSearchRequestDto;
-import com.goorm.tablepick.domain.restaurant.dto.response.CategoryResponseDto;
-import com.goorm.tablepick.domain.restaurant.dto.response.PagedRestaurantResponseDto;
-import com.goorm.tablepick.domain.restaurant.dto.response.RestaurantDetailResponseDto;
-import com.goorm.tablepick.domain.restaurant.dto.response.RestaurantResponseDto;
-import com.goorm.tablepick.domain.restaurant.dto.response.RestaurantSearchResponseDto;
+import com.goorm.tablepick.domain.restaurant.dto.response.*;
 import com.goorm.tablepick.domain.restaurant.entity.Restaurant;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -24,5 +20,5 @@ public interface RestaurantService {
 
     List<CategoryResponseDto> getCategoryList();
 
-    List<RestaurantSearchResponseDto> searchRestaurantsV1(RestaurantSearchRequestDto requestDto);
+    PagedRestaurantSearchResponseDto searchRestaurantsV1(RestaurantSearchRequestDto requestDto);
 }
